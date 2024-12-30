e875e66a07a65e2c42c7ef3d32f6bf3dbfe51047
Remzo00 <gusinacremzo00@gmail.com>
2024-12-30 08:26:22 +0100

    add sockets

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,19 +1,19 @@
 export const socketHandlers = (io) => {
   io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+    console.log(`User connected: ${socket.id}`);
 
-    socket.on("joinRoom", (room) => {
-      socket.join(room);
-      console.log(`User ${socket.id} joined room ${room}`);
+    socket.on("send_message", (data) => {
+      console.log("Message received:", data);
+      o.to(data.room).emit("receive_message", data);
     });
 
-    socket.on("sendMessage", ({ room, message }) => {
-      console.log(`Message to room ${room}:`, message);
-      io.to(room).emit("receiveMessage", { room, message });
+    socket.on("join_room", (room) => {
+      socket.join(room);
+      console.log(`User ${socket.id} joined room: ${room}`);
     });
 
     socket.on("disconnect", () => {
-      console.log("User disconnected:", socket.id);
+      console.log(`User disconnected: ${socket.id}`);
     });
   });
 };
