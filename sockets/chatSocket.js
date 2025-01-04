67fcc010b16782fdd3603b3a130b3d0be03146ed
Remzo00 <gusinacremzo00@gmail.com>
2025-01-04 12:25:42 +0100

    add encrypted messages

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -3,17 +3,33 @@ export const socketHandlers = (io) => {
     console.log(`User connected: ${socket.id}`);
 
     socket.on("send_message", (data) => {
-      console.log("Received message data:", data);
+      const base64EncryptedData = Buffer.from(data.encryptedData).toString(
+        "base64"
+      );
+      const base64IV = Buffer.from(data.iv).toString("base64");
+      console.log("Encrypted message received:", {
+        encryptedData: base64EncryptedData,
+        iv: base64IV,
+        room: data.room,
+        senderName: data.senderName,
+      });
 
       const messageData = {
-        ...data,
+        encryptedData: data.encryptedData,
+        iv: data.iv,
+        room: data.room,
         senderId: socket.id,
         senderName: data.senderName,
       };
 
-      socket.broadcast.to(data.room).emit("receive_message", messageData, {
-        senderId: socket.id,
-        senderName: data.senderName,
+      socket.broadcast.to(data.room).emit("receive_message", messageData);
+    });
+
+    socket.on("share_key", (data) => {
+      console.log(`Key shared in room ${data.room}:`, data.key);
+      socket.broadcast.to(data.room).emit("receive_key", {
+        room: data.room,
+        key: data.key,
       });
     });
 
